99e42ebdd3e72d67c0f36fd9d6948b9813c0b2ca
nicolasgalban13-dotcom <nicolasgalban13@gmail.com>
2026-08-20 03:22:28 -0300

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
+// Servir index.html en la raíz
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 app.use(express.static('.'));
 
 const storage = multer.diskStorage({
